a9611b2f275ea5c2f96849923e705caa555d5eda
Kefan-S <shenkefan1990@gmail.com>
2020-01-15 10:29:26 +0800

    [Kefan] - Rename the package, and remove the comment in the pom file.

--- src/main/java/io/jenkins/plugins/collector/HelloWorldBuilder.java
+++ src/main/java/io/jenkins/plugins/collector/HelloWorldBuilder.java
@@ -1,4 +1,4 @@
-package io.jenkins.plugins.sample;
+package io.jenkins.plugins.collector;
 
 import hudson.Launcher;
 import hudson.Extension;
--- src/test/java/io/jenkins/plugins/collector/HelloWorldBuilderTest.java
+++ src/test/java/io/jenkins/plugins/collector/HelloWorldBuilderTest.java
@@ -1,4 +1,4 @@
-package io.jenkins.plugins.sample;
+package io.jenkins.plugins.collector;
 
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
