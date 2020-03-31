cab28b3c94947bfdb9c1db53ebb34cb8489a8d89
EternalSaul <kxin@thoughtworks.com>
2020-03-31 10:26:19 +0800

    [kxin/linlin.fu]: extract some constant

--- src/main/java/io/jenkins/plugins/collector/config/Constant.java
+++ src/main/java/io/jenkins/plugins/collector/config/Constant.java
@@ -1,8 +1,8 @@
 package io.jenkins.plugins.collector.config;
 
 public interface Constant {
-    String FULLNAME = "builds";
-    String NAMESPACE = "default";
-    String SUBSYSTEM = "jenkins";
-    String[] LABEL_NAME_ARRAY = {"jenkins_job"};
+    String METRICS_NAME_PREFIX = "builds";
+    String METRICS_NAMESPACE = "default";
+    String METRICS_SUBSYSTEM = "jenkins";
+    String[] METRICS_LABEL_NAME_ARRAY = {"jenkins_job"};
 }
