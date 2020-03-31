894bb464c43d2022253a14d6a7cbdd85107c87a2
EternalSaul <kxin@thoughtworks.com>
2020-03-31 11:17:26 +0800

    [kxin/linlin.fu]: refactor java by stream api

--- src/main/java/io/jenkins/plugins/collector/util/Jobs.java
+++ src/main/java/io/jenkins/plugins/collector/util/Jobs.java
@@ -1,35 +1,24 @@
 package io.jenkins.plugins.collector.util;
 
 import hudson.model.Item;
+import hudson.model.ItemGroup;
 import hudson.model.Job;
-import hudson.model.Run;
 import jenkins.model.Jenkins;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class Jobs {
 
   public static void forEachJob(Consumer<Job> consumer) {
-
-    Jenkins jenkins = Jenkins.getInstanceOrNull();
-    if (jenkins == null) {
-      return;
-    }
-    List<Item> items = jenkins.getAllItems();
-    if (items != null) {
-      for (Item item : items) {
-        Collection<? extends Job> jobs = item.getAllJobs();
-        if (jobs != null) {
-          for (Job job : jobs) {
-            if (job != null) {
-              consumer.accept(job);
-            }
-          }
-        }
-      }
-    }
+    Optional.ofNullable(Jenkins.getInstanceOrNull())
+            .map(ItemGroup::getAllItems)
+            .map(Collection::stream)
+            .orElse(Stream.empty())
+            .map(Item::getAllJobs)
+            .flatMap(Collection::stream)
+            .forEach(consumer);
   }
 }
