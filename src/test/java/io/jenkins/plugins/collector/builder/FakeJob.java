52adf424cd545c39d8577e07242a0c4fa3b8582c
linlifu2 <linlin.fu@thoughtworks.com>
2020-03-31 19:26:50 +0800

    add test module and fake build and job

---
+++ src/test/java/io/jenkins/plugins/collector/builder/FakeJob.java
@@ -0,0 +1,49 @@
+package io.jenkins.plugins.collector.builder;
+
+import hudson.model.Job;
+import hudson.model.Run;
+import hudson.model.TopLevelItem;
+import hudson.model.TopLevelItemDescriptor;
+import org.junit.Rule;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import java.io.IOException;
+import java.util.SortedMap;
+
+public class FakeJob extends Job implements TopLevelItem {
+
+  @Rule
+  public static JenkinsRule rule = new JenkinsRule();
+
+  int i;
+  private final SortedMap<Integer, FakeBuild> runs;
+
+  public FakeJob(SortedMap<Integer, FakeBuild> runs) {
+    super(rule.jenkins, "name");
+    this.runs = runs;
+    i = 1;
+  }
+
+  @Override
+  public int assignBuildNumber() throws IOException {
+    return i++;
+  }
+
+  @Override
+  public SortedMap<Integer, ? extends Run> _getRuns() {
+    return runs;
+  }
+
+  @Override
+  public boolean isBuildable() {
+    return true;
+  }
+
+  @Override
+  protected void removeRun(Run run) {
+  }
+
+  public TopLevelItemDescriptor getDescriptor() {
+    throw new AssertionError();
+  }
+}
