dd18acb73527b1a8263201754090cec6d5089b91
EternalSaul <kxin@thoughtworks.com>
2020-04-02 11:19:54 +0800

    [kxin/linlin.fu]: create MockBuildBuilder,MockBuild

---
+++ src/test/java/io/jenkins/plugins/collector/builder/MockBuild.java
@@ -0,0 +1,44 @@
+package io.jenkins.plugins.collector.builder;
+
+import hudson.model.Job;
+import hudson.model.Result;
+import hudson.model.Run;
+import org.mockito.Mockito;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+
+public class MockBuild extends Run {
+    protected MockBuild(@Nonnull Job job) throws IOException {
+        super(job);
+    }
+
+    MockBuild createPreviousBuild(long interval, long duration, Result result) {
+        if (interval <= 0 || duration <= 0 || (getStartTimeInMillis() - interval <=0)) {
+            throw new IllegalArgumentException();
+        }
+        MockBuild previousBuild = new MockBuildBuilder()
+                .startTimeInMillis(getStartTimeInMillis() - interval)
+                .duration(duration)
+                .nextBuild(this)
+                .result(result)
+                .create();
+        Mockito.when(this.getPreviousBuild()).thenReturn(previousBuild);
+        return previousBuild;
+    }
+
+    MockBuild createNextBuild(long interval, long duration, Result result) {
+        if (interval <= 0 || duration <= 0 ) {
+            throw new IllegalArgumentException();
+        }
+        MockBuild nextBuild = new MockBuildBuilder()
+                .startTimeInMillis(getStartTimeInMillis() + interval)
+                .duration(duration)
+                .previousBuild(this)
+                .result(result)
+                .create();
+        Mockito.when(this.getNextBuild()).thenReturn(nextBuild);
+        return nextBuild;
+    }
+
+}
---
+++ src/test/java/io/jenkins/plugins/collector/builder/MockBuildBuilder.java
@@ -0,0 +1,48 @@
+package io.jenkins.plugins.collector.builder;
+
+import hudson.model.Result;
+import hudson.model.Run;
+import org.mockito.Mockito;
+
+public class MockBuildBuilder {
+    Run previousBuild;
+    Run nextBuild;
+    Result result;
+    long duration;
+    long startTimeInMillis;
+
+    public MockBuildBuilder previousBuild(Run previousBuild) {
+        this.previousBuild = previousBuild;
+        return this;
+    }
+
+    public MockBuildBuilder nextBuild(Run nextBuild) {
+        this.nextBuild = nextBuild;
+        return this;
+    }
+
+    public MockBuildBuilder result(Result result) {
+        this.result = result;
+        return this;
+    }
+
+    public MockBuildBuilder duration(long duration) {
+        this.duration = duration;
+        return this;
+    }
+
+    public MockBuildBuilder startTimeInMillis(long startTimeInMillis) {
+        this.startTimeInMillis = startTimeInMillis;
+        return this;
+    }
+
+    public MockBuild create() {
+        MockBuild mockBuild = Mockito.mock(MockBuild.class);
+        Mockito.when(mockBuild.getStartTimeInMillis()).thenReturn(startTimeInMillis);
+        Mockito.when(mockBuild.getResult()).thenReturn(result);
+        Mockito.when(mockBuild.getDuration()).thenReturn(duration);
+        Mockito.when(mockBuild.getNextBuild()).thenReturn(nextBuild);
+        Mockito.when(mockBuild.getPreviousBuild()).thenReturn(previousBuild);
+        return mockBuild;
+    }
+}
