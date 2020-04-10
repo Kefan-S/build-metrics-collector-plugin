e63e44dade75bab46c417fd436b699b33ca9b51e
EternalSaul <kxin@thoughtworks.com>
2020-04-10 15:52:34 +0800

    [kxin]: rm useless code

--- src/test/java/io/jenkins/plugins/collector/config/PrometheusConfigurationTest.java
+++ src/test/java/io/jenkins/plugins/collector/config/PrometheusConfigurationTest.java
@@ -24,7 +24,7 @@ import static org.mockito.Mockito.times;
 public class PrometheusConfigurationTest {
 
   @Before
-  public void setUp() throws Exception {
+  public void setUp() {
     PowerMockito.mockStatic(Jenkins.class);
     Jenkins jenkins = mock(Jenkins.class);
     Mockito.when(jenkins.getRootDir()).thenReturn(new File("."));
