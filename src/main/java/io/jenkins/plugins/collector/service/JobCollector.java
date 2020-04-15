da933cfd7032f4d581914d638557101ac3019dda
tangmingheng <mhtang@thoughtworks.com>
2020-04-15 15:53:11 +0800

    set default metrics when build doesn't exist

--- src/main/java/io/jenkins/plugins/collector/service/JobCollector.java
+++ src/main/java/io/jenkins/plugins/collector/service/JobCollector.java
@@ -2,21 +2,41 @@ package io.jenkins.plugins.collector.service;
 
 
 import com.google.inject.Inject;
+import com.google.inject.name.Named;
 import hudson.model.Run;
 import io.jenkins.plugins.collector.data.BuildProvider;
 import io.prometheus.client.Collector;
+import io.prometheus.client.Gauge;
 import io.prometheus.client.SimpleCollector;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static java.util.stream.Collectors.toList;
+import static org.apache.commons.collections.CollectionUtils.isEmpty;
 
 public class JobCollector extends Collector {
 
   private final List<Function<Run, List<SimpleCollector>>> buildHandler;
 
   private BuildProvider buildProvider;
+
+  @Inject
+  @Named("durationGauge")
+  private Gauge durationGaugeMetric;
+
+  @Inject
+  @Named("leadTimeGauge")
+  private Gauge leadTimeGaugeMetric;
+
+  @Inject
+  @Named("recoverTimeGauge")
+  private Gauge recoverTimeGaugeMetric;
+
+  @Inject
+  @Named("startTimeGauge")
+  private Gauge startTimeGaugeMetric;
 
   @Inject
   public JobCollector(List<Function<Run, List<SimpleCollector>>> buildHandler,
@@ -56,6 +76,12 @@ public class JobCollector extends Collector {
   }
 
   private List<MetricFamilySamples> getJobMetric(List<SimpleCollector> collectors) {
+    if (isEmpty(collectors)) {
+      return newArrayList(durationGaugeMetric, leadTimeGaugeMetric, recoverTimeGaugeMetric, startTimeGaugeMetric).stream()
+          .map(Collector::collect)
+          .flatMap(Collection::stream)
+          .collect(toList());
+    }
     return collectors.stream()
         .map(Collector::collect)
         .flatMap(Collection::stream)
