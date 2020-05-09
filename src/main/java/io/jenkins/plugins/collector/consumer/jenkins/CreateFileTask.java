08fa627802e6ae892333e986598c8b491b0ecc6f
linlifu2 <linlin.fu@thoughtworks.com>
2020-05-09 16:21:32 +0800

    [lin] add test for adapter and format some code

--- src/main/java/io/jenkins/plugins/collector/consumer/jenkins/CreateFileTask.java
+++ src/main/java/io/jenkins/plugins/collector/consumer/jenkins/CreateFileTask.java
@@ -11,43 +11,44 @@ import org.jenkinsci.remoting.RoleChecker;
 
 @EqualsAndHashCode
 public class CreateFileTask implements Serializable, Callable<Boolean, IOException> {
-    private static final long serialVersionUID = 1L;
-    private final String fileContent;
-    private final String fileName;
-    private final String folderPath;
 
-    CreateFileTask(String folderPath, String fileName, String fileContent) {
-      this.folderPath = folderPath;
-      this.fileName = fileName;
-      this.fileContent = fileContent;
-    }
+  private static final long serialVersionUID = 1L;
+  private final String fileContent;
+  private final String fileName;
+  private final String folderPath;
 
-    @Override
-    public void checkRoles(RoleChecker roleChecker) throws SecurityException {
-      roleChecker.check(this, Roles.SLAVE);
-    }
+  CreateFileTask(String folderPath, String fileName, String fileContent) {
+    this.folderPath = folderPath;
+    this.fileName = fileName;
+    this.fileContent = fileContent;
+  }
 
-    @Override
-    public Boolean call() throws IOException {
-      try {
-        FilePath folder = new FilePath(new File(folderPath));
-        FilePath textFile = new FilePath(new File(folderPath+fileName));
-        String finalFileContent = "";
-        String eol = System.getProperty("line.separator");
+  @Override
+  public void checkRoles(RoleChecker roleChecker) throws SecurityException {
+    roleChecker.check(this, Roles.SLAVE);
+  }
 
-        if (!textFile.exists()) {
-          finalFileContent = fileContent;
-        } else {
-          String existingFileContents = textFile.readToString();
-          finalFileContent = existingFileContents.concat(eol + fileContent);
-        }
-        textFile.deleteContents();
-        finalFileContent = finalFileContent.replaceAll("\n", System.lineSeparator());
-        folder.mkdirs();
-        textFile.write(finalFileContent, "UTF-8");
-      } catch (Exception e) {
-        throw new IOException(e.getMessage());
+  @Override
+  public Boolean call() throws IOException {
+    try {
+      FilePath folder = new FilePath(new File(folderPath));
+      FilePath textFile = new FilePath(new File(folderPath + fileName));
+      String finalFileContent = "";
+      String eol = System.getProperty("line.separator");
+
+      if (!textFile.exists()) {
+        finalFileContent = fileContent;
+      } else {
+        String existingFileContents = textFile.readToString();
+        finalFileContent = existingFileContents.concat(eol + fileContent);
       }
-      return true;
+      textFile.deleteContents();
+      finalFileContent = finalFileContent.replaceAll("\n", System.lineSeparator());
+      folder.mkdirs();
+      textFile.write(finalFileContent, "UTF-8");
+    } catch (Exception e) {
+      throw new IOException(e.getMessage());
     }
+    return true;
+  }
 }
--- src/main/java/io/jenkins/plugins/collector/model/BuildInfoResponse.java
+++ src/main/java/io/jenkins/plugins/collector/model/BuildInfoResponse.java
@@ -8,10 +8,11 @@ import lombok.Data;
 @Data
 @Builder
 public class BuildInfoResponse {
+
   private BigDecimal failureRate;
   private Integer deploymentFrequency;
   private List<Long> startTime;
   private List<Long> duration;
   private List<Long> leadTime;
   private List<Long> recoverTime;
- }
+}
