4564e2eeef783073a781935347da6a7d2cc57181
EternalSaul <kxin@thoughtworks.com>
2020-05-11 15:08:14 +0800

    xxx

--- src/main/webapp/scripts/opal-dashboard/opal-chart-generator.js
+++ src/main/webapp/scripts/opal-dashboard/opal-chart-generator.js
@@ -1,40 +1,56 @@
-function lineChartOpationGenerator(chartName, xAxisData, yAxisData) {
+let durationcalculate = function (value) {
+  var millisecond = new Number(value);
+  var days = parseInt(millisecond / (1000 * 60 * 60 * 24));
+  var hours = parseInt((millisecond % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = parseInt((millisecond % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = (millisecond % (1000 * 60)) / 1000;
+  var time = ""
+  if(days) time = time + days + " 天 ";
+  if(hours) time = time + hours + " 小时 ";
+  if(minutes) time = time + minutes + " 分 ";
+  if(seconds) time = time + seconds + " 秒 ";
+  return time;
+};
+
+let timeStampTranslator = {
+  formatter: function (value, index) {
+    // 格式化成月/日，只在第一个刻度显示年份
+    console.warn(value)
+    var date = new Date(new Number(value) * 1000);
+    var texts = [(date.getMonth() + 1), date.getDate()];
+    if (index === 0) {
+      texts.unshift(1900 + date.getYear());
+    }
+    return texts.join('/');
+  }
+};
+
+function lineChartOpationGenerator(chartName, xAxisData, yAxisData,
+    yAxisName , xAxisName = "Start Time") {
   return {
     title: {
       text: chartName
     },
-    xAxis: {
-      type: 'category',
-      data: xAxisData,
-      axisLabel: {
-        formatter: function (value, index) {
-          // 格式化成月/日，只在第一个刻度显示年份
-          console.warn(value)
-          var date = new Date(new Number(value) * 1000);
-          var texts = [(date.getMonth() + 1), date.getDate()];
-          if (index === 0) {
-            texts.unshift(1900 + date.getYear());
-          }
-          return texts.join('/');
-        }
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        animation: false
+      },
+      formatter: function (params) {
+        return 'X: ' + params.data[0].toFixed(2) + '<br>Y: ' + params.data[1].toFixed(2);
       }
     },
+    xAxis: {
+      name: xAxisName,
+      type: 'category',
+      data: xAxisData,
+      axisLabel: timeStampTranslator
+    },
     yAxis: {
+      name: yAxisName,
       type: 'value',
       axisLabel: {
-        formatter: function (value, index) {
-          var millisecond = new Number(value);
-          var days = parseInt(millisecond / (1000 * 60 * 60 * 24));
-          var hours = parseInt((millisecond % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-          var minutes = parseInt((millisecond % (1000 * 60 * 60)) / (1000 * 60));
-          var seconds = (millisecond % (1000 * 60)) / 1000;
-          var time = ""
-          if(days) time = time + days + " 天 ";
-          if(hours) time = time + hours + " 小时 ";
-          if(minutes) time = time + minutes + " 分 ";
-          if(seconds) time = time + seconds + " 秒 ";
-          return time;
-        }
+        formatter: durationcalculate
       }
     },
     series: [{
