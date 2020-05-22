b2a112c6c2afb9d14f87c062b3db8d714cdfff01
linlifu2 <linlin.fu@thoughtworks.com>
2020-05-22 15:01:36 +0800

    [lin] change the build info csv file path to job root path

--- src/test/java/io/jenkins/plugins/collector/consumer/jenkins/JenkinsConsumerTest.java
+++ src/test/java/io/jenkins/plugins/collector/consumer/jenkins/JenkinsConsumerTest.java
@@ -1,5 +1,6 @@
 package io.jenkins.plugins.collector.consumer.jenkins;
 
+import hudson.model.TopLevelItem;
 import hudson.remoting.VirtualChannel;
 import io.jenkins.plugins.collector.model.BuildInfo;
 import io.jenkins.plugins.collector.model.JenkinsFilterParameter;
@@ -17,6 +18,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import static com.google.common.collect.Lists.newArrayList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -26,27 +28,31 @@ public class JenkinsConsumerTest {
   Jenkins jenkins;
   @Mock
   VirtualChannel channel;
+  @Mock
+  TopLevelItem item;
   @Captor
   private ArgumentCaptor<CreateFileTask> taskArgumentCaptor;
 
   @Test
   public void should_call_create_task_when_call_accept_given_build_info_list() throws IOException, InterruptedException {
-    when(jenkins.getRootDir()).thenReturn(new File("/folder"));
-    when(jenkins.getChannel()).thenReturn(channel);
-
-    JenkinsConsumer consumer = new JenkinsConsumer(jenkins);
     BuildInfo test = BuildInfo.builder().triggerInfo(new TriggerInfo()).jenkinsJob("test").build();
+    when(jenkins.getChannel()).thenReturn(channel);
+    when(jenkins.getItem(test.getJenkinsJob())).thenReturn(item);
+    when(item.getRootDir()).thenReturn(new File("/folder"));
+    JenkinsConsumer consumer = new JenkinsConsumer(jenkins);
+
     consumer.accept(newArrayList(test));
 
     verify(channel).call(taskArgumentCaptor.capture());
     CreateFileTask task = taskArgumentCaptor.getValue();
-    CreateFileTask expected = new CreateFileTask("/folder/cache4Opal/", "test", "null,null,null,null,test,null,null,null,null");
+    CreateFileTask expected = new CreateFileTask("/folder",  "null,null,null,null,test,null,null,null,null");
     assertEquals(expected, task);
   }
 
   @Test
   public void should_return_null_when_getMetrics_given_not_exist_job_name() {
-    when(jenkins.getRootDir()).thenReturn(new File("/folder"));
+    when(jenkins.getItem(any())).thenReturn(item);
+    when(item.getRootDir()).thenReturn(new File("/folder"));
     JenkinsFilterParameter jenkinsFilterParameter = JenkinsFilterParameter
         .builder().build();
 
