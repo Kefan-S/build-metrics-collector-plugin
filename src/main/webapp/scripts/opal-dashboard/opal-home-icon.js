0915b5eb5d1dee05c794dd57935001d2fb908503
EternalSaul <kxin@thoughtworks.com>
2020-06-02 17:03:25 +0800

    [kxin]: add skip to home event

--- src/main/webapp/scripts/opal-dashboard/opal-home-icon.js
+++ src/main/webapp/scripts/opal-dashboard/opal-home-icon.js
@@ -1,6 +1,6 @@
 Vue.component('opal-home-icon', {
   template: `
-          <div class="opal-home-page-icon" title="Home Page" onclick="goHome()">
+          <div class="opal-home-page-icon" title="Home Page" @click="goHome()">
             <Icon type="md-home" size="32"/>
           </div>
     `,
